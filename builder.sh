2cb007f998892da6d5dba2c94507654e889edf68
jn <tupiszakaczki@gmail.com>
2017-07-28 08:55:44 -0400

    cleaned builder

--- builder.sh
+++ builder.sh
@@ -4,3 +4,17 @@ pdflatex GeoPAT2_Manual
 # bibtex GeoPAT2_Manual
 pdflatex GeoPAT2_Manual
 pdflatex GeoPAT2_Manual
+
+# move pdf
+
+mv GeoPAT2_Manual.pdf output/
+
+# clean temporary files
+
+rm GeoPAT2_Manual.dvi
+rm GeoPAT2_Manual.ps
+rm GeoPAT2_Manual.aux
+rm GeoPAT2_Manual.log
+rm GeoPAT2_Manual.bbl
+rm GeoPAT2_Manual.blg
+rm GeoPAT2_Manual.toc
