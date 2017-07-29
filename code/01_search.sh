941c30c8852112e5af8c103d574e0f073d8299ba
jn <tupiszakaczki@gmail.com>
2017-07-29 13:30:25 -0400

    search example in manual

--- code/01_search.sh
+++ code/01_search.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+gpat_gridhis -i Augusta2011.tif -o grid -s cooc -z 50 -f 50 -n pdf
+
+gpat_pointshis -i Augusta2011.tif -o query_signatures.txt -s cooc -z 50 -n pdf --xy_file=coordinates.txt
+
+gpat_search -i grid -r quesry_signatures.txt
+
+gpat_pointsts -i grid -o query_signatures.txt --xy_file=coordinates.txt
