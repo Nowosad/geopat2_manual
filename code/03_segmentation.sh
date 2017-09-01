70f55bcd341f8534242ce3b33cae69135b8acf52
jn <tupiszakaczki@gmail.com>
2017-09-01 18:48:15 -0400

    100 -> 50

--- code/03_segmentation.sh
+++ code/03_segmentation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100
-gpat_segment -i Augusta2011_grid100 -o Augusta2011_seg100.tif -v Augusta2011_seg100.shp
-gpat_segquality -i Augusta2011_grid100 -s Augusta2011_seg100.tif -g Augusta2011_seg100_inh.tif -o Augusta2011_seg100_ins.tif
+gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid50 -z 50 -f 50
+gpat_segment -i Augusta2011_grid50 -o Augusta2011_seg50.tif -v Augusta2011_seg50.shp
+gpat_segquality -i Augusta2011_grid50 -s Augusta2011_seg50.tif -g Augusta2011_seg50_inh.tif -o Augusta2011_seg50_ins.tif
