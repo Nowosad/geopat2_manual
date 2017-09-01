75bbf4767eaa655d843f3f29e5c1f5f9499a9233
jn <tupiszakaczki@gmail.com>
2017-09-01 18:45:13 -0400

    example to 100

--- code/03_segmentation.sh
+++ code/03_segmentation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid15 -z 15 -f 15
-gpat_segment -i Augusta2011_grid15 -o Augusta2011_seg15.tif -v Augusta2011_seg15.shp
-gpat_segquality -i Augusta2011_grid15 -s Augusta2011_seg15.tif -g Augusta2011_seg15_inh.tif -o Augusta2011_seg15_ins.tif
+gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100
+gpat_segment -i Augusta2011_grid100 -o Augusta2011_seg100.tif -v Augusta2011_seg100.shp
+gpat_segquality -i Augusta2011_grid100 -s Augusta2011_seg100.tif -g Augusta2011_seg100_inh.tif -o Augusta2011_seg100_ins.tif
