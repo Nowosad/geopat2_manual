3db9a7548e51deaf413c3c5b9789b5241d424204
jn <tupiszakaczki@gmail.com>
2017-09-01 18:44:13 -0400

    segmentation example

--- code/03_segmentation.sh
+++ code/03_segmentation.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid15 -z 15 -f 15
+gpat_segment -i Augusta2011_grid15 -o Augusta2011_seg15.tif -v Augusta2011_seg15.shp
+gpat_segquality -i Augusta2011_grid15 -s Augusta2011_seg15.tif -g Augusta2011_seg15_inh.tif -o Augusta2011_seg15_ins.tif
