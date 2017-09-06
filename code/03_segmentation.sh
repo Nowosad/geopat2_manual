775acd387ec72d345c96b1c0662d5da0382bd5f0
jn <tupiszakaczki@gmail.com>
2017-09-06 12:17:41 +0100

    segmentation code template

--- code/03_segmentation.sh
+++ code/03_segmentation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## spatial
 gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid50 -z 50 -f 50
 gpat_segment -i Augusta2011_grid50 -o Augusta2011_seg50.tif -v Augusta2011_seg50.shp
 gpat_segquality -i Augusta2011_grid50 -s Augusta2011_seg50.tif -g Augusta2011_seg50_inh.tif -o Augusta2011_seg50_ins.tif
+
+## temporal
