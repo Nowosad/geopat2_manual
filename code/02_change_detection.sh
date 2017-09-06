4ed255c0de247b50d640d1781bad7bf27f0add36
jn <tupiszakaczki@gmail.com>
2017-09-06 12:02:41 +0100

    change detection code

--- code/02_change_detection.sh
+++ code/02_change_detection.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+gpat_gridhis -i Augusta2006.tif -o Augusta2006_grid50 -z 50 -f 50
+gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid50 -z 50 -f 50
+gpat_compare -i Augusta2006_grid50 -i Augusta2011_grid50 -o Augusta0611_compared.tif
