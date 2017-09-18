98cdcedc27b171277cb02b308903eb5408c61ec5
jn <tupiszakaczki@gmail.com>
2017-09-18 13:40:54 +0200

    change det plots

--- code/00_augusta2006.R
+++ code/00_augusta2006.R
@@ -3,7 +3,12 @@ library(raster)
 augusta2006_all = raster("~/Tmp/nlcd_2006_landcover_2011_edition_2014_10_10/nlcd_2006_landcover_2011_edition_2014_10_10.img")
 augusta2011 = raster("data/Augusta2011.tif")
 
-crop(augusta2006_all,
-                   augusta2011,
-                   filename = "data/Augusta2006.tif",
-                   datatype = "INT1U")
+augusta2006 = crop(augusta2006_all,
+     augusta2011)
+
+resample(augusta2006,
+         augusta2011,
+         method = "ngb",
+         filename = "data/Augusta2006.tif",
+         datatype = "INT1U", 
+         overwrite = TRUE)
--- code/02_change_detection.R
+++ code/02_change_detection.R
@@ -6,11 +6,35 @@ library(tidyverse)
 dir.create("tmp")
 file.copy(from = "data/Augusta2011.tif", to = "tmp")
 file.copy(from = "data/Augusta2006.tif", to = "tmp")
+file.copy(from = "data/nlcd_colors.txt", to = "tmp")
 
 setwd("tmp/")
 
 ## create a first figure ----------------------------------------------------
 # two images (2006 and 2011)
+
+augusta2011 = raster("Augusta2011.tif") %>% 
+        as.factor()
+
+augusta2006 = raster("Augusta2006.tif") %>% 
+        as.factor()
+
+rat = levels(augusta2011)[[1]]
+rat[["landcoveaugusta"]] = rat$ID
+
+levels(augusta2011) = rat
+levels(augusta2006) = rat
+
+lc_colors = read_delim('nlcd_colors.txt', col_names = FALSE, delim = " ") %>% 
+        mutate(hex=rgb(X2, X3, X4, , maxColorValue = 255)) %>% 
+        filter(X1 %in% c(rat$ID))
+
+augusta_stack = stack(augusta2006, augusta2011)
+
+png("../figs/change_det_two_maps.png", width = 800, height = 300)
+levelplot(augusta_stack, col.regions=lc_colors$hex, margin=FALSE,
+          xlab=NULL, ylab=NULL, colorkey=FALSE, scales=list(draw=FALSE))
+dev.off()
 
 ## calculate the change ------------------------------------------------------
 system("gpat_gridhis -i Augusta2006.tif -o Augusta2006_grid50 -z 50 -f 50")
