6974aba5939ae6d542664226631771b3ef1e3af1
jn <tupiszakaczki@gmail.com>
2017-09-19 14:38:58 +0200

    ts seg plots

--- code/03_segmentation_ts.R
+++ code/03_segmentation_ts.R
@@ -13,22 +13,28 @@ setwd("tmp/")
 ## norm data ------------------------------------------------------------------
 system("gpat_gridts -i GB_pr01.tif -i GB_pr02.tif -i GB_pr03.tif -i GB_pr04.tif -i GB_pr05.tif -i GB_pr06.tif -i GB_pr07.tif -i GB_pr08.tif -i GB_pr09.tif -i GB_pr10.tif -i GB_pr11.tif -i GB_pr12.tif -o GB_pr_grid -n")
 system("gpat_segment -i GB_pr_grid -o GB_pr_seg.tif -v GB_pr_seg.shp -m tsEUC --lthreshold=0.5 --uthreshold=1")
-system("gpat_segquality -i GB_pr_grid -s GB_pr_seg.tif -g GB_pr_seg_inh.tif -o GB_pr_seg_ins.tif -m tsEUC")
+system("gpat_segquality -i GB_pr_grid -s GB_pr_seg.tif -g GB_pr_seg_ih.tif -o GB_pr_seg_is.tif -m tsEUC")
 
 ## segmentation plot -------------------------------------------------------
 segm = st_read("GB_pr_seg.shp")
-segplot = tmap::qtm(segm)
-segplot
+segm$segment_id[segm$segment_id == -2147483648] = NA
+
+library(tmap)
+segplot = tm_shape(segm) +
+        tm_borders()
+
+save_tmap(segplot, filename = "../figs/ts_seg.png",
+          height = 500, width = 390, scale = 0.5)
 
 ## quality plots ------------------------------------------------------------
-inh = raster("GB_pr_seg_inh.tif")
-ins = raster("GB_pr_seg_ins.tif")
+inh = raster("GB_pr_seg_ih.tif")
+ins = raster("GB_pr_seg_is.tif")
 
-inh_plot = levelplot(inh, margin = FALSE, main = "Inhomogeneity")
-inh_plot
+in_rasters = stack(inh, ins)
 
-ins_plot = levelplot(ins, margin = FALSE, main = "Isolation")
-ins_plot
+png("../figs/ts_segmentation_ihis.png", width = 800, height = 300)
+levelplot(in_rasters, names.attr=c("Inhomogeneity", "Isolation"))
+dev.off()
 
 ## calculation of a segmentation quality -------------------------------------
 seq_qual = function(inh, ins){
@@ -36,8 +42,10 @@ seq_qual = function(inh, ins){
 }
 
 qual = overlay(inh, ins, fun = seq_qual)
-qual_plot = levelplot(qual, margin = FALSE, main = "Quality")
-qual_plot
+
+png("../figs/ts_segmentation_qualityall.png", width = 500, height = 450)
+levelplot(qual, margin = FALSE, main = "Quality")
+dev.off()
 
 ## overall quality ------------------------------------------------------------
 cellStats(qual, "mean", na.rm = TRUE)
