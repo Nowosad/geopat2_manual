613b20bb40f1408f419c3437c37a1f6c2b4d34d7
jn <tupiszakaczki@gmail.com>
2017-09-19 15:26:34 +0200

    seg size fix

--- code/04_clustering_polygon.R
+++ code/04_clustering_polygon.R
@@ -6,13 +6,13 @@ library(tidyverse)
 ## files prep ----------------------------------------------------------------
 dir.create("tmp")
 file.copy(from = "data/Augusta2011.tif", to = "tmp")
-file.copy(from = "data/Augusta2011_seg50.tif", to = "tmp")
+file.copy(from = "data/Augusta2011_seg100.tif", to = "tmp")
 file.copy(from = "data/nlcd_colors.txt", to = "tmp")
 
 setwd("tmp/")
 
 ## gpat clustering  prep -----------------------------------------------------
-system("gpat_polygon -i Augusta2011.tif -e Augusta2011_seg50.tif -o Augusta2011_psign.txt")
+system("gpat_polygon -i Augusta2011.tif -e Augusta2011_seg100.tif -o Augusta2011_psign.txt")
 system("gpat_distmtx -i Augusta2011_psign.txt -o Augusta2011_matrix4.csv")
 
 ## r clustering -------------------------------------------------------------
