ff3c30b15f5418b1260b0186016be98bb20740eb
jn <tupiszakaczki@gmail.com>
2017-09-25 09:41:12 +0200

    small code fix

--- code/04_clustering_polygon.R
+++ code/04_clustering_polygon.R
@@ -12,11 +12,12 @@ file.copy(from = "data/nlcd_colors.txt", to = "tmp")
 setwd("tmp/")
 
 ## gpat clustering  prep -----------------------------------------------------
+system("")
 system("gpat_polygon -i Augusta2011.tif -e Augusta2011_seg100.tif -o Augusta2011_psign.txt")
 system("gpat_distmtx -i Augusta2011_psign.txt -o Augusta2011_matrix_seg.csv")
 
 ## r clustering -------------------------------------------------------------
-dist_matrix = read.csv("Augusta2011_matrix4.csv")[, -1] %>% as.dist()
+dist_matrix = read.csv("Augusta2011_matrix_seg.csv")[, -1] %>% as.dist()
 hclust_result = hclust(d = dist_matrix, method = "ward.D")
 plot(hclust_result)
 
