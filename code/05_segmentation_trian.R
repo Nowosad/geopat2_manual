26a9cd71f897da97a4b2d7979fe05969dc374ee2
Nowosad <tupiszakaczki@gmail.com>
2017-10-11 14:12:08 -0400

    segmenataion train test

---
+++ code/05_segmentation_trian.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(sf)
+library(raster)
+library(rasterVis)
+
+## files prep ----------------------------------------------------------------
+dir.create("tmp")
+file.copy(from = "data/Augusta2011.tif", to = "tmp")
+file.copy(from = "data/nlcd_colors.txt", to = "tmp")
+
+setwd("tmp/")
+
+## the first figure ---------------------------------------------------------
+augusta2011 = raster("Augusta2011.tif") %>% 
+        as.factor()
+
+rat = levels(augusta2011)[[1]]
+rat[["landcoveaugusta2011"]] = rat$ID
+levels(augusta2011) = rat
+
+lc_colors = read_delim('nlcd_colors.txt', col_names = FALSE, delim = " ") %>% 
+        mutate(hex=rgb(X2, X3, X4, , maxColorValue = 255)) %>% 
+        filter(X1 %in% rat$ID)
+
+## the code ------------------------------------------------------------------
+system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100")
+system("gpat_segment -i Augusta2011_grid100 -o Augusta2011_seg100.tif -v Augusta2011_seg100.shp --lthreshold=0.1 --uthreshold=0.3 -m jsd")
+system("gpat_segment -i Augusta2011_grid100 -o Augusta2011_seg100t.tif -v Augusta2011_seg100t.shp --lthreshold=0.1 --uthreshold=0.3 -m trian")
+
+## segmentation plot -------------------------------------------------------
+segm = st_read("Augusta2011_seg100.shp")
+segm_trian = st_read("Augusta2011_seg100t.shp")
+
+detach(package:ggplot2)
+raster_seg_plot = levelplot(augusta2011, col.regions=lc_colors$hex, margin=FALSE,
+                            xlab=NULL, ylab=NULL, colorkey=FALSE, scales=list(draw=FALSE)) +
+        layer(sp.lines(as(segm, "Spatial"), lwd=4, col='black'))
+
+raster_seg_plot_trian = levelplot(augusta2011, col.regions=lc_colors$hex, margin=FALSE,
+                            xlab=NULL, ylab=NULL, colorkey=FALSE, scales=list(draw=FALSE)) +
+        layer(sp.lines(as(segm_trian, "Spatial"), lwd=4, col='black'))
+
+raster_seg_plot
+raster_seg_plot_trian
+
+## the end --------------------------------------------------------------------
+setwd("..")
+unlink("tmp", recursive = TRUE, force = TRUE)
