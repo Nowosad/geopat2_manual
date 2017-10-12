478a52b126bd30338ca3734aa06e2a9bc723f4ba
Nowosad <tupiszakaczki@gmail.com>
2017-10-12 13:09:39 -0400

    working (and slow) header remover

--- code/07_grd2txt.R
+++ code/07_grd2txt.R
@@ -11,5 +11,26 @@ file.copy(from = "data/nlcd_colors.txt", to = "tmp")
 setwd("tmp/")
 
 ## the code ------------------------------------------------------------------
-system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100")
-system("gpat_grd2txt -i Augusta2011_grid100 -o Augusta2011_grid100.txt -z 100 -f 100")
+system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100 -s prod")
+system("gpat_grd2txt -i Augusta2011_grid100 -o Augusta2011_grid100.txt")
+
+grid100 = read_lines("Augusta2011_grid100.txt")
+
+header_remover = function(x){
+        new_vector = x %>% 
+                gsub("(?<=\\[)(.*)(?=>)", "", ., perl = TRUE) %>% 
+                gsub("\\[> ", "", ., perl = TRUE)
+        
+        
+}
+
+grid100 %>% map(~header_remover(.)) %>% 
+        flatten() %>% 
+        write_lines(., "Augusta2011_grid100c.txt")
+
+grid100c = read_csv("Augusta2011_grid100c.txt", col_names = FALSE)
+
+## the end --------------------------------------------------------------------
+setwd("..")
+unlink("tmp", recursive = TRUE, force = TRUE)
+
