c51cf22605b60427089a6be21dce323606cdfdd7
Nowosad <tupiszakaczki@gmail.com>
2017-10-23 12:07:14 -0400

    updated clustering example

--- code/04_clustering_grid.R
+++ code/04_clustering_grid.R
@@ -2,6 +2,7 @@ library(sf)
 library(raster)
 library(rasterVis)
 library(tidyverse)
+library(rgeopat2)
 
 ## files prep ----------------------------------------------------------------
 dir.create("tmp")
@@ -19,12 +20,15 @@ system("gpat_distmtx -i Augusta2011_grid100.txt -o Augusta2011_matrix_grid.csv")
 dist_matrix = read.csv("Augusta2011_matrix_grid.csv")[, -1] %>% as.dist()
 hclust_result = hclust(d = dist_matrix, method = "ward.D")
 plot(hclust_result)
-
 hclust_cut = cutree(hclust_result, 5)
 
-# module for grid raster creation!!
-
 ## return to map -----------------------------------------------------------
+my_classes = data.frame(class = hclust_cut)
+
+my_grid = gpat_gridcreate("Augusta2011_grid100.hdr")
+my_grid = st_as_sf(my_classes, my_grid)
+
+plot(my_grid)
 
 ## clean --------------------------------------------------------------------
 setwd("..")
