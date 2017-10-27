aa74c7f15b21927a5f10fdcd6093de4b4e61ea43
Nowosad <tupiszakaczki@gmail.com>
2017-10-27 15:41:21 -0400

    topology visualization

---
+++ code/10_topology_visalization.R
@@ -0,0 +1,48 @@
+library(rgeopat2)
+library(sf)
+library(tidyverse)
+library(raster)
+library(rasterVis)
+
+## files prep ----------------------------------------------------------------
+dir.create("tmp")
+file.copy(from = "data/Augusta2011.tif", to = "tmp")
+file.copy(from = "data/nlcd_colors.txt", to = "tmp")
+
+setwd("tmp/")
+
+## the first figure ---------------------------------------------------------
+augusta2011 = raster("Augusta2011.tif") %>% 
+        as.factor()
+
+rat = levels(augusta2011)[[1]]
+rat[["landcoveaugusta2011"]] = rat$ID
+levels(augusta2011) = rat
+
+lc_colors = read_delim('nlcd_colors.txt', col_names = FALSE, delim = " ") %>% 
+        mutate(hex=rgb(X2, X3, X4, , maxColorValue = 255)) %>% 
+        filter(X1 %in% rat$ID)
+
+## the code ------------------------------------------------------------------
+system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100")
+
+## new grids ------------------------------------------------------------------
+header_filepath = "Augusta2011_grid100.hdr"
+my_grid = gpat_gridcreate(header_filepath)
+my_grid_brick = gpat_gridcreate(header_filepath, brick = TRUE)
+
+detach(package:ggplot2)
+augusta = levelplot(augusta2011, col.regions=lc_colors$hex, margin=FALSE,
+                       xlab=NULL, ylab=NULL, colorkey=FALSE, scales=list(draw=FALSE))
+
+grid1plot = augusta + 
+        layer(sp.polygons(as(my_grid, "Spatial"), lwd=4, col='black'))
+grid1plot
+
+grid2plot = augusta + 
+        layer(sp.polygons(as(my_grid_brick, "Spatial"), lwd=4, col='black'))
+grid2plot
+
+## the end --------------------------------------------------------------------
+setwd("..")
+unlink("tmp", recursive = TRUE, force = TRUE)
