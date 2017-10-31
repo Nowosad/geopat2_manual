37be3793c043357c68c0af2b8daa8b3712ea4bc6
Nowosad <tupiszakaczki@gmail.com>
2017-10-31 11:19:28 -0400

    next tests

--- code/11_size_shift_test.R
+++ code/11_size_shift_test.R
@@ -33,12 +33,15 @@ system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100d -z 100 -f 10")
 ### visuals -------------------------------------------------------------------
 header_filepath100a = "Augusta2011_grid100a.hdr"
 header_filepath100c = "Augusta2011_grid100c.hdr"
+header_filepath100d = "Augusta2011_grid100d.hdr"
 
 my_grid100a = gpat_gridcreate(header_filepath100a)
-my_grid100c = gpat_gridcreate(header_filepath)
+my_grid100c = gpat_gridcreate(header_filepath100c)
+my_grid100d = gpat_gridcreate(header_filepath100d)
 
 my_grid_brick100a = gpat_gridcreate(header_filepath100a, brick = TRUE)
 my_grid_brick100c = gpat_gridcreate(header_filepath100c, brick = TRUE)
+my_grid_brick100d = gpat_gridcreate(header_filepath100d, brick = TRUE)
 
 detach(package:ggplot2)
 augusta = levelplot(augusta2011, col.regions=lc_colors$hex, margin=FALSE,
@@ -46,9 +49,10 @@ augusta = levelplot(augusta2011, col.regions=lc_colors$hex, margin=FALSE,
 
 augusta + layer(sp.polygons(as(my_grid100a, "Spatial"), lwd=4, col='black'))
 augusta + layer(sp.polygons(as(my_grid100c, "Spatial"), lwd=4, col='black'))
+augusta + layer(sp.polygons(as(my_grid100d, "Spatial"), lwd=4, col='black'))
 augusta + layer(sp.polygons(as(my_grid_brick100a, "Spatial"), lwd=4, col='black'))
 augusta + layer(sp.polygons(as(my_grid_brick100c, "Spatial"), lwd=4, col='black'))
-
+augusta + layer(sp.polygons(as(my_grid_brick100d, "Spatial"), lwd=4, col='black'))
 
 ## the end --------------------------------------------------------------------
 setwd("..")
