9c076b71ee316d0553f013e607248a0d43c2b5f0
jn <tupiszakaczki@gmail.com>
2018-09-22 14:27:53 +0200

    signature change

--- code/01_search_polygon.R
+++ code/01_search_polygon.R
@@ -11,10 +11,10 @@ file.copy(from = "data/nlcd_colors.txt", to = "tmp")
 setwd("tmp/")
 
 ## temp solution 2 ----------------------------------------------------------
-system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100")
+system("gpat_gridhis -i Augusta2011.tif -o Augusta2011_grid100 -z 100 -f 100 -s cooc")
 system("gpat_segment -i Augusta2011_grid100 -o Augusta2011_seg100.tif -v Augusta2011_seg100.gpkg --lthreshold=0.1 --uthreshold=0.3")
 system("gdalwarp -tr 30 30 Augusta2011_seg100.tif Augusta2011_seg100_res.tif")
-system("gpat_polygon -i Augusta2011.tif -e Augusta2011_seg100_res.tif -o Augusta2011_psign.txt")
+system("gpat_polygon -i Augusta2011.tif -e Augusta2011_seg100_res.tif -o Augusta2011_psign.txt -s cooc")
 # search
 system("gpat_search -i Augusta2011_grid100 -r Augusta2011_psign.txt")
 
@@ -39,3 +39,4 @@ levelplot(
 ## clean ---------------------------------------------------------------------
 setwd("..")
 unlink("tmp", recursive = TRUE, force = TRUE)
+
